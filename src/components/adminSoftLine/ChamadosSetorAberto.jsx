3d2318a46ded2cc5a9722f73acae17d8e1956b27
Ibra_Co <52585726+Ibra1993@users.noreply.github.com>
2025-04-12 11:08:50 -0300

    Update ChamadosSetorAberto.jsx

--- src/components/adminSoftLine/ChamadosSetorAberto.jsx
+++ src/components/adminSoftLine/ChamadosSetorAberto.jsx
@@ -1,16 +1,12 @@
-
 import React, { useEffect, useState } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import axios from "axios";
 import AdminHeaders from "../headers/AdminHeaders";
-import { useNavigate } from "react-router-dom"; // Importe o useNavigate
+import { useNavigate } from "react-router-dom";
 
-
- const  ChamadosSetorAberto = () =>{
-
-const navigate = useNavigate(); // Hook para redirecionamento
-
+const ChamadosSetorAberto = () => {
+  const navigate = useNavigate();
   const [chartData, setChartData] = useState({
     suporte: 0,
     cobranca: 0,
@@ -27,92 +23,48 @@ const navigate = useNavigate(); // Hook para redirecionamento
     sugestao: 0,
     cobrancaHomologacao: 0,
     resolucaoDeErro: 0
-
-   
   });
 
-{/*}
-React.useEffect(() => {
-  axios
-    .get("https://chamados-softline-k3bsb.ondigitalocean.app/chamados/relatorio/statusAssuntosChamados") // URL correta da API
-    .then((response) => {
-      const data = response.data; // Obtenha os dados diretamente
-      setChartData({
-        suporte: data.suporte || 0,
-        cobranca: data.cobranca || 0,
-        comercial: data.comercial || 0,
-        implantacao: data.implantacao || 0,
-        fiscal: data.fiscal || 0,
-        customizacao: data.customizacao || 0,
-        elogio: data.elogio || 0,
-        outros: data.outros || 0,
-        reclamacao: data.reclamacao || 0,
-        urgente: data.urgente || 0,
-        visitaTecnica: data.visitaTecnica || 0,
-        treinamento: data.treinamento || 0,
-        sugestao: data.sugestao || 0,
-        cobrancaHomologacao: data.cobrancaHomologacao || 0,
-        resolucaoDeErro: data.resolucaoDeErro || 0,
-
-      });
-    })
-    .catch((error) => console.error("Erro ao carregar os dados:", error));
-}, []);
-*/}
-
-
-
-  // Verifica se o usuário está autenticado ao carregar o componente
-    useEffect(() => {
-        const token = localStorage.getItem("token"); // Recupera o token do localStorage
-        if (!token) {
-            navigate("/nao-autorizado"); // Redireciona para a página de não autorizado
-        } else {
-            // Faz a requisição para carregar os dados do relatório
-            axios
-                .get("https://chamados-softline-k3bsb.ondigitalocean.app/chamados/relatorio/statusAssuntosChamados", {
-                    headers: {
-                        Authorization: `Bearer ${token}`, // Envia o token no cabeçalho
-                    },
-                })
-                .then((response) => {
-                    const data = response.data; // Obtenha os dados diretamente
-                    setChartData({
-                        suporte: data.suporte || 0,
-                        cobranca: data.cobranca || 0,
-                        comercial: data.comercial || 0,
-                        implantacao: data.implantacao || 0,
-                        fiscal: data.fiscal || 0,
-                        customizacao: data.customizacao || 0,
-                        elogio: data.elogio || 0,
-                        outros: data.outros || 0,
-                        reclamacao: data.reclamacao || 0,
-                        urgente: data.urgente || 0,
-                        visitaTecnica: data.visitaTecnica || 0,
-                        treinamento: data.treinamento || 0,
-                        sugestao: data.sugestao || 0,
-                        cobrancaHomologacao: data.cobrancaHomologacao || 0,
-                        resolucaoDeErro: data.resolucaoDeErro || 0,
-
-                     
-                    });
-                })
-                .catch((error) => {
-                    if (error.response && (error.response.status === 403 || error.response.status === 401)) {
-                        navigate("/nao-autorizado"); // Redireciona para a página de não autorizado
-                    } else {
-                        console.error("Erro ao carregar os dados:", error);
-                    }
-                });
-        }
-    }, [navigate]);
-
-
-
-
-
-
-
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/nao-autorizado");
+    } else {
+      axios
+        .get("https://chamados-softline-k3bsb.ondigitalocean.app/chamados/relatorio/statusAssuntosChamados", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          const data = response.data;
+          setChartData({
+            suporte: data.suporte || 0,
+            cobranca: data.cobranca || 0,
+            comercial: data.comercial || 0,
+            implantacao: data.implantacao || 0,
+            fiscal: data.fiscal || 0,
+            customizacao: data.customizacao || 0,
+            elogio: data.elogio || 0,
+            outros: data.outros || 0,
+            reclamacao: data.reclamacao || 0,
+            urgente: data.urgente || 0,
+            visitaTecnica: data.visitaTecnica || 0,
+            treinamento: data.treinamento || 0,
+            sugestao: data.sugestao || 0,
+            cobrancaHomologacao: data.cobrancaHomologacao || 0,
+            resolucaoDeErro: data.resolucaoDeErro || 0,
+          });
+        })
+        .catch((error) => {
+          if (error.response && (error.response.status === 403 || error.response.status === 401)) {
+            navigate("/nao-autorizado");
+          } else {
+            console.error("Erro ao carregar os dados:", error);
+          }
+        });
+    }
+  }, [navigate]);
 
   const options = {
     chart: {
@@ -122,119 +74,53 @@ React.useEffect(() => {
       text: "Relatório dos Status de todos os Chamados",
     },
     tooltip: {
-      valueSuffix: "%",
+      pointFormat: '{series.name}: <b>{point.y} ({point.percentage:.1f}%)</b>'
     },
     plotOptions: {
-      pie: { // Adicione "pie" aqui para configurar gráficos do tipo pizza
+      pie: {
         allowPointSelect: true,
         cursor: "pointer",
         dataLabels: {
           enabled: true,
-          format: "{point.name}", // Exibe apenas o nome da categoria
+          format: "<b>{point.name}</b>: {point.y}",
           style: {
             fontSize: "1.2em",
             textOutline: "none",
           },
         },
+        showInLegend: true
       },
     },
-
-
-
-    series: [
-      {
-        name: "Porcentagem",
-        colorByPoint: true,
-        data: [
-          {
-            name: "Chamados de Suporte",
-            y: chartData.suporte,
-          },
-          {
-            name: "Chamados de cobrança",
-            sliced: true,
-            selected: true,
-            y: chartData.cobranca,
-          },
-          {
-            name: "Chamados de comercial",
-            y: chartData.comercial,
-          },
-
-           {
-              name: "Chamados de implantacao",
-              y: chartData.implantacao,
-           },
-
-        {
-                     name: "Chamados de fiscal",
-                     y: chartData.fiscal,
-        },
-
-     {
-                  name: "Chamados de customizacao",
-                  y: chartData.customizacao,
-     },
-
-     {
-                   name: "Elogios",
-                   y: chartData.elogio,
-      },
-
-        {
-                     name: "Outros",
-                     y: chartData.outros,
-        },
-
-        {
-                         name: "Reclamações",
-                         y: chartData.reclamacao,
-        },
-
-        {
-                         name: "Urgente",
-                         y: chartData.urgente,
-        },
-
-        {
-                             name: "Visita Técnica",
-                             y: chartData.visitaTecnica,
-        },
-       {
-                                 name: "Treinamentos",
-                                 y: chartData.treinamento,
-       },
-
-      {
-                                name: "Sugestões",
-                                y: chartData.sugestao,
-      },
-
-     {
-                               name: "Cobrança Homologação",
-                               y: chartData.cobrancaHomologacao,
-     },
-
-     {
-                                name: "Resolução de Erro",
-                                y: chartData.resolucaoDeErro,
-      },
-
-         
-
-        ],
-      },
-    ],
+    series: [{
+      name: "Quantidade",
+      colorByPoint: true,
+      data: [
+        { name: "Suporte", y: chartData.suporte },
+        { name: "Cobrança", y: chartData.cobranca },
+        { name: "Comercial", y: chartData.comercial },
+        { name: "Implantação", y: chartData.implantacao },
+        { name: "Fiscal", y: chartData.fiscal },
+        { name: "Customização", y: chartData.customizacao },
+        { name: "Elogio", y: chartData.elogio },
+        { name: "Outros", y: chartData.outros },
+        { name: "Reclamação", y: chartData.reclamacao },
+        { name: "Urgente", y: chartData.urgente },
+        { name: "Visita Técnica", y: chartData.visitaTecnica },
+        { name: "Treinamento", y: chartData.treinamento },
+        { name: "Sugestão", y: chartData.sugestao },
+        { name: "Cobrança Homologação", y: chartData.cobrancaHomologacao },
+        { name: "Resolução de Erro", y: chartData.resolucaoDeErro }
+      ]
+    }]
   };
 
   return (
     <div>
-        <AdminHeaders />
-        <h2 className="alert alert-info text-center" role="alert">Relatório de todos os chamados dos setores em aberto</h2>
+      <AdminHeaders />
+      <h2 className="alert alert-info text-center" role="alert">
+        Relatório de todos os chamados dos setores em aberto
+      </h2>
 
-
-
-      {/* Highcharts */}
       <div style={{ height: "400px" }}>
         <HighchartsReact highcharts={Highcharts} options={options} />
       </div>
@@ -243,4 +129,3 @@ React.useEffect(() => {
 };
 
 export default ChamadosSetorAberto;
-
