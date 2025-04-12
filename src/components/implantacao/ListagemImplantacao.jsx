241f7f4657994237cae7ec739cfff6b82f07289e
Ibra_Co <52585726+Ibra1993@users.noreply.github.com>
2025-04-12 13:27:10 -0300

    Update ListagemImplantacao.jsx

--- src/components/implantacao/ListagemImplantacao.jsx
+++ src/components/implantacao/ListagemImplantacao.jsx
@@ -348,11 +348,16 @@ const fetchOptions = async () => {
                setPaginaAtual(0); // Resetar para a primeira página ao mudar o tamanho
              }}
            >
-             <option value="5">5</option>
-             <option value="10">10</option>
-             <option value="20">20</option>
-             <option value="50">50</option>
-           </select>
+    <option value="5">5</option>
+    <option value="10">10</option>
+    <option value="20">20</option>
+    <option value="50">50</option>
+    <option value="60">60</option>
+    <option value="70">70</option>
+    <option value="80">80</option>
+    <option value="100">100</option>
+    <option value="350">350</option>
+ </select>
 </div>
 
        
